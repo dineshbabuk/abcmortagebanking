cde32b1c1de9c9b883f83f4e593b621b9a59afe2
hackathon <hackathon@JAVAUI3007.p53b32fww1yedmclu3kq4lcvre.bx.internal.cloudapp.net>
2019-09-27 15:25:54 +0000

    controller

--- src/main/java/com/rest/application/controller/BankingController.java
+++ src/main/java/com/rest/application/controller/BankingController.java
@@ -5,22 +5,46 @@ import java.util.List;
 
 import javax.websocket.server.PathParam;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rest.application.service.BankingService;
+import com.rest.application.service.BankingServiceImpl;
+
+/**
+ * Controller class for handling account related functions
+ * 
+ * @author Dinesh
+ * @author Pamela
+ *
+ */
 @RestController
+@RequestMapping("/mortage")
 public class BankingController
 {
-
+@Autowired
+BankingService bankingServiceImpl;
+	
 	@RequestMapping(method = RequestMethod.GET, value = "/usersearch/{criteria}")
-	@ResponseBody
-	public List<String> searchAllUsers(@PathParam("criteria") String criteria)
+	public List<String> searchAllUsers(@PathVariable(value = "criteria") String criteria)
 	{
 		List<String> bookDetails = new ArrayList<>();
 		System.out.println("criteria ::" + criteria);
 		return bookDetails;
 	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/create/{accountNumber}")
+	public String createMortagageAccount(@PathVariable(value = "accountNumber") String accountNumber) throws Exception
+	{
+		return bankingServiceImpl.createMortagageAccount(accountNumber);
+		
+		
+		
+	}
+
 
 }
