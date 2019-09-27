af5d3817d7f255f53a90c413c484bd914efb7e3c
dineshbabuK <dineshbabuKandasamy@gmail.com>
2019-09-27 16:40:12 +0530

    banking controller

--- src/main/java/com/rest/application/controller/BankingController.java
+++ src/main/java/com/rest/application/controller/BankingController.java
@@ -1,9 +1,26 @@
 package com.rest.application.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BankingController
 {
+
+	@RequestMapping(method = RequestMethod.GET, value = "/usersearch/{criteria}")
+	@ResponseBody
+	public List<String> searchAllUsers(@PathParam("criteria") String criteria)
+	{
+		List<String> bookDetails = new ArrayList<>();
+		System.out.println("criteria ::" + criteria);
+		return bookDetails;
+	}
 
 }
